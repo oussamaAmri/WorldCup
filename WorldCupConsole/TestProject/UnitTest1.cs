8c89fa1a5a30e603e6b11a8f68f32dfa5299327b
Kevin Sousselier <kevin.sousselier@gmail.com>
2022-12-27 13:50:17 +0100

    Some new exercices

--- WorldCupConsole/TestProject/UnitTest1.cs
+++ WorldCupConsole/TestProject/UnitTest1.cs
@@ -53,4 +53,96 @@ public class UnitTest1
 
         Check.That(result).IsEqualTo(output);
     }
+
+    [Fact]
+    public void CreateList()
+    {
+        var result = Algo.CreateList();
+
+        Check.That(result).ContainsExactly(1, 2, 3);
+    }
+
+    [Fact]
+    public void AddElementToList()
+    {
+        var input = new List<int> { 1, 2, 3 };
+
+        var result = Algo.AddElementToList(input);
+
+        Check.That(result).ContainsExactly(1, 2, 3, 4);
+    }
+
+    [Fact]
+    public void AddElementToArray()
+    {
+        var input = new int[]{ 1, 2, 3 };
+
+        var result = Algo.AddElementToArray(input);
+
+        Check.That(result).ContainsExactly(1, 2, 3, 4);
+    }
+
+    [Fact]
+    public void FilterDictionaryByKey()
+    {
+        var input = new Dictionary<int, string>
+        {
+            [1] = "Red",
+            [2] = "Blue",
+            [3] = "Green",
+            [4] = "Orange",
+            [5] = "Pink",
+        };
+
+        var result = Algo.FilterDictionaryByKey(input);
+
+        Check.That(result).ContainsExactly(KeyValuePair.Create(5, "Pink"));
+    }
+
+    [Fact]
+    public void FilterDictionaryByValue()
+    {
+        var input = new Dictionary<int, string>
+        {
+            [1] = "Red",
+            [2] = "Red",
+            [3] = "Green",
+            [4] = "Green",
+            [5] = "Green",
+        };
+
+        var result = Algo.FilterDictionaryByKey(input);
+
+        Check.That(result).ContainsExactly(KeyValuePair.Create(1, "Red"), KeyValuePair.Create(2, "Red"));
+    }
+
+    [Fact]
+    public void ToDictionary()
+    {
+        var input = new List<string> { "Green", "Red", "Blue", "Red", "Green", "Purple", "Green" };
+
+        var result = Algo.ToDictionary(input);
+
+        var dicToCheck = result.OrderBy(x => x.Key).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+        Check.That(dicToCheck).ContainsExactly(
+            KeyValuePair.Create("Blue", 1),
+            KeyValuePair.Create("Green", 3),
+            KeyValuePair.Create("Red", 2),
+            KeyValuePair.Create("Purple", 1));
+    }
+
+    [Theory]
+    [InlineData(0, 7)]
+    [InlineData(1, 14)]
+    [InlineData(2, 15)]
+    [InlineData(3, 11)]
+    [InlineData(4, 4)]
+    public void Sum(int index, int output)
+    {
+        var input = new int[] { 2, 5, 7, 3, 1 };
+
+        var result = Algo.Sum(input, index);
+
+        Check.That(result).IsEqualTo(output);
+    }
 }
--- WorldCupConsole/WorldCupConsole/Algo.cs
+++ WorldCupConsole/WorldCupConsole/Algo.cs
@@ -1,4 +1,6 @@
-﻿namespace WorldCupConsole;
+﻿using System.Globalization;
+
+namespace WorldCupConsole;
 
 public static class Algo
 {
@@ -22,6 +24,48 @@ public static class Algo
 
     // Compute Fibonacci
     public static int Fibonacci(int input)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Create a list
+    public static List<int> CreateList()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Add element to a list
+    public static List<int> AddElementToList(List<int> input)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Add element to an array
+    public static int[] AddElementToArray(int[] input)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Filter dictionary by key >= 5
+    public static Dictionary<int, string> FilterDictionaryByKey(Dictionary<int, string> input)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Filter dictionary by value == Red
+    public static Dictionary<int, string> FilterDictionaryByValue(Dictionary<int, string> input)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Create dictionary
+    public static Dictionary<string, int> ToDictionary(List<string> input)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Return input[index - 1] + input[index] + input[index + 1]
+    public static int Sum(int[] input, int index)
     {
         throw new NotImplementedException();
     }
